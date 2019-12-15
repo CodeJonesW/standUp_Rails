554d618cf3528209cdb9f95edd0f909a220ed3e5
Will Jones <willjones2010@gmail.com>
2019-12-14 22:38:28 -0500

    controller functions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,48 @@
 class CompaniesController < ApplicationController
+    def index
+        @companies = Company.all
+        render json: @companies
+    end
+
+    def create
+        @company = Company.create(a_params)
+        company = UserSerializer.new(@company)
+        render json: {company: company}
+    end
+
+    
+    def show
+        begin @company = Company.find(params[:id])
+            render json: @company
+        rescue
+            render json: {status: "error", code: 404, message: "Company does not exist"}
+        end
+    end
+
+
+    def destroy
+       if  @company = Company.destroy(params[:id])
+            company = CompanySerializer.new(@company)
+            render json: {company: company}
+       end
+    end
+
+    def update
+        @company = Company.find(params[:id])
+        if @company.update(a_params)
+            # user = UserSerializer.new(@user)
+            # render json: {user: user}
+            company = CompanySerializer.new(@company)
+            render json: {company: company}
+        else
+            render json: @company.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+	def a_params
+		params.require(:company).permit(:name, :about, :private, :location)
+    end
 end
