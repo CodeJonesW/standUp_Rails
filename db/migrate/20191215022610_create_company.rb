45ab58a4d55ec124d56ffd2102961fb43de55334
Will Jones <willjones2010@gmail.com>
2019-12-14 21:40:19 -0500

    companies table created

---
+++ db/migrate/20191215022610_create_company.rb
@@ -0,0 +1,11 @@
+class CreateCompany < ActiveRecord::Migration[5.2]
+  def change
+    create_table :companies do |t|
+          t.string :name
+          t.string :about
+          t.string :location
+          t.boolean :private, default: false
+          t.timestamps
+    end
+  end
+end
